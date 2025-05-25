3d07cc31c4919f6b2829921c890ca89911f89454
Chiwuikem1 <corji@villanova.edu>
2025-05-25 12:26:43 -0400

    added home.js for future functionality

--- slink-app/src/App.js
+++ slink-app/src/App.js
@@ -1,43 +1,11 @@
 // App.js
 import './App.css';
-import { useAuth } from "react-oidc-context";
+import Home from './defaultScreen/home';
 
 function App() {
-  const auth = useAuth();
-
-  const signOutRedirect = () => {
-    const clientId = "68fk1ld7rql13b1qufr7q30v1n";
-    const logoutUri = "http://localhost:3000";
-    const cognitoDomain = "https://us-east-2rf2to41xj.auth.us-east-2.amazoncognito.com";
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-  };
-
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (auth.error) {
-    return <div>Encountering error... {auth.error.message}</div>;
-  }
-
-  if (auth.isAuthenticated) {
-    return (
-      <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
-        <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-        <button onClick={() => auth.removeUser()}>Sign out</button>
-      </div>
-    );
-  }
 
   return (
-    <div>
-      <button onClick={() => auth.signinRedirect()} className='signup'>Sign in</button>
-      <button onClick={() => signOutRedirect()}>Sign out</button>
-    </div>
+    <Home />
   );
 }
 
